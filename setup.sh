2940097a2761590589a333541fb7bf948fc581b7
Lior (leo) Hakim <liorhakim@gmail.com>
2019-01-24 16:34:39 +0200

    -y

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@ apps=(
 	zsh
 )
 
-sudo apt install "${apps[@]}"
+sudo apt install -y "${apps[@]}"
 # apt install python-pip python-dev build-essential
 
 wget https://github.com/jingweno/ccat/releases/download/v1.1.0/linux-amd64-1.1.0.tar.gz -O /tmp/ccat-linux-amd64-1.1.0.tar.gz
