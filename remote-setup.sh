afa4c4c76b2f7b4ee9d1a7821927896dc65753e0
0i0 <liorhakim@gmail.com>
2021-02-26 06:47:17 +0200

    better install

--- remote-setup.sh
+++ remote-setup.sh
@@ -1,11 +1,14 @@
-#!/usr/bin/env bash
-
-# [[ -x `command -v wget` ]] && CMD="wget --no-check-certificate -O -"
-# [[ -x `command -v curl` ]] >/dev/null 2>&1 && CMD="curl -#L"
-CMD="curl -#L"
+#!/bin/sh
+CMD="curl -L"
 if [ -z "$CMD" ]; then
   echo "No curl or wget available. Aborting."
 else
+  echo -e "\x1b[36m ______     __  __     ______     __  __     __      "
+  echo -e "\x1b[32m/\x1b[36m\  __ \ \x1b[32m  /\x1b[36m\ \_\ \ \x1b[32m  /\x1b[36m\  __ \ \x1b[32m  /\x1b[36m\ \_\ \ \x1b[32m  /\x1b[36m\ \     "
+  echo -e "\x1b[32m\ \x1b[36m\  __ \ \x1b[32m \ \x1b[36m\  __ \ \x1b[32m \ \x1b[36m\ \\\\\x1b[32m/\x1b[36m\ \ \x1b[32m \ \x1b[36m\____ \ \x1b[32m \ \x1b[36m\_\    "
+  echo -e "\x1b[32m \ \x1b[36m\_\ \_\ \x1b[32m \ \x1b[36m\_\ \_\ \x1b[32m \ \x1b[36m\_____\ \x1b[32m \/\x1b[36m\_____\ \x1b[32m \/\x1b[36m\_\   "
+  echo -e "\x1b[32m  \/_/\/_/   \/_/\/_/   \/_____/   \/_____/   \/_/   "
+  echo -e ""
   echo "Installing dotfiles-server"
   mkdir -p "$HOME/dotfiles-server" && \
   eval "$CMD https://github.com/0i0/dotfiles-server/tarball/master | tar -xzv -C ~/dotfiles-server --strip-components=1 --exclude='{.gitignore}'"
